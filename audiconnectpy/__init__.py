c96fc8b418c0ecf22d9b99ab97cb554c4651370b
cyr-ius <cyr-ius@ipocus.net>
2023-02-12 15:29:40 +0100

    Remove custom logging

--- audiconnectpy/__init__.py
+++ audiconnectpy/__init__.py
@@ -3,9 +3,5 @@
 """audiconnectpy package."""
 from .api import AudiConnect
 from .exceptions import AudiException, AuthorizationError
-from .util import addLoggingLevel
 
 __all__ = ["AudiConnect", "AudiException", "AuthorizationError"]
-
-addLoggingLevel("advanced", 9)
-addLoggingLevel("expert", 8)
