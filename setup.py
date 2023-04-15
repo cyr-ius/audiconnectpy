f297862a3cf650718d181f4f8ee6fd50431b3505
cyr-ius <cyr-ius@ipocus.net>
2023-04-15 15:22:04 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -1,31 +1,13 @@
 # -*- coding: utf-8 -*-
 """Setup package."""
 
-import codecs
 import os
-import re
 
 from setuptools import find_packages, setup
 
 # Method for retrieving the version is taken from the setup.py of pip itself:
 # https://github.com/pypa/pip/blob/master/setup.py
 here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    """Read file."""
-    with codecs.open(os.path.join(here, *parts), "r") as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    """Find version."""
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
 
 # Get the long description from the README file
 with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
@@ -38,10 +20,10 @@ setup(
     packages=find_packages(),
     author="cyr-ius",
     author_email="cyr-ius@ipocus.net",
-    description="Provides asynchronous authentication and access",
+    description="Provides asynchronous authentication and access to Audi Connect",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["aiohttp>=3.8.1", "voluptuous>=0.13.1", "beautifulsoup4>=4.11.2"],
+    install_requires=["aiohttp>=3.8.1", "beautifulsoup4>=4.11.2"],
     license="GPL-3",
     include_package_data=True,
     url="https://github.com/cyr-ius/audiconnectpy/tree/master/audiconnectpy",
