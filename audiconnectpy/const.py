a469be6a075003e6df4ffe20162322f1ca7d6875
cyr-ius <cyr-ius@ipocus.net>
2023-12-28 16:38:26 +0100

    Add constants file

---
+++ audiconnectpy/const.py
@@ -0,0 +1,12 @@
+"""Constants."""
+BRAND = "Audi"
+FAILED = "failed"
+MAX_RESPONSE_ATTEMPTS = 10
+REQUEST_FAILED = "request_failed"
+REQUEST_STATUS_SLEEP = 10
+REQUEST_SUCCESSFUL = "request_successful"
+SUCCEEDED = "succeeded"
+URL_HOME_REGION = "https://msg.volkswagen.de/fs-car"
+URL_HOME_REGION_SETTER = "https://mal-1a.prd.ece.vwg-connect.com/api"
+URL_INFO_VEHICLE = "https://app-api.live-my.audi.com/vgql/v1/graphql"
+URL_INFO_VEHICLE_US = "https://app-api.my.aoa.audi.com/vgql/v1/graphql"
