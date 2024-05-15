3f0667a3147504f3bc6f584e8e8ad8d58dacbc50
cyr-ius <cyr-ius@ipocus.net>
2024-05-15 18:08:33 +0200

    Add load fixture

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,9 @@
-"""Initialize."""
+"""Tests for audiconnect."""
+
+from pathlib import Path
+
+
+def load_fixture(filename: str) -> str:
+    """Load a fixture."""
+    path = Path(__package__) / "fixtures" / filename
+    return path.read_text(encoding="utf-8")
