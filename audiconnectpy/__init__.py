9191c7a61e30bd700a73a294248cbf82f2bfb348
cyr-ius <cyr-ius@ipocus.net>
2024-05-20 19:00:56 +0200

    Add moels constants

--- audiconnectpy/__init__.py
+++ audiconnectpy/__init__.py
@@ -1,6 +1,7 @@
 """audiconnectpy package."""
 
 from .api import AudiConnect
+from .const import MODELS
 from .exceptions import AudiException, AuthorizationError
 
-__all__ = ["AudiConnect", "AudiException", "AuthorizationError"]
+__all__ = ["AudiConnect", "AudiException", "AuthorizationError", "MODELS"]
